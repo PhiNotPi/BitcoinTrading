389985f89b8200840f5f9e6a48afb054bcc26d4f
PhiNotPi <mrclarinetplayer@gmail.com>
2015-10-14 23:44:22 -0400

    Update controller.pl

--- controller.pl
+++ controller.pl
@@ -12,7 +12,18 @@ $dsn = "DBI:mysql:database=bitcoin;host=$dbhost;port=$dbport";
 $dbh = DBI->connect($dsn, $dbuser, $dbpw);
 
 for(1..800){
+    $dbh->do("CREATE TABLE IF NOT EXISTS randbot (time BIGINT(20), value DOUBLE)");
     $dbh->do("CREATE TABLE IF NOT EXISTS BTC (time BIGINT(20), value DOUBLE)");
+    $btc = 0.5;
+    $usd = 1-$btc;
+    $btcn = `perl randbot.pl`;
+    $usdn = 1-$btcn;
+    if($btcn > $btc){
+        $btcn -= 0.02($btcn - $btc);
+    }
+    elsif($usdn > $usd){
+        $usdn -= 0.02($usdn - $usd);
+    }
     my $content = get($url);
     $dbh->do("INSERT INTO BTC VALUES (?, ?)", undef, time, $content);
     sleep(20);
